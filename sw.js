b5c3305464f8ae84715172ca2f47bce283d1e8d5
mutaznraghad-creator <mutaznraghad@gmail.com>
2025-11-01 22:05:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,3 @@
-/* Simple PWA cache */
-const CACHE = 'sb-cache-v1';
-const ASSETS = [
-  '/', '/index.html', '/manifest.webmanifest',
-  '/icons/icon-192.png','/icons/icon-256.png','/icons/icon-384.png','/icons/icon-512.png'
-];
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e)=>{
-  const req = e.request;
-  if(req.method!=='GET') return;
-  e.respondWith(
-    caches.match(req).then(cached=>{
-      const fetchPromise = fetch(req).then(res=>{
-        const copy = res.clone();
-        caches.open(CACHE).then(c=>c.put(req, copy));
-        return res;
-      }).catch(()=>cached || caches.match('/index.html'));
-      return cached || fetchPromise;
-    })
-  );
-});
+const CACHE_VERSION = 'sb-v3-2025-11-01';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => clients.claim());
